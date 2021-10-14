f71ad2e771ea0b157fbde1688b069b34826408f9
Nick Bechtel <bechtel.nick@gmail.com>
2021-10-14 09:52:13 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (equation) => (equation = 2000 / 100);
+
+const square = (sq) => {
+  sq = 2;
+  return sq * sq;
+};
+
+const add = (a, b) => {
+  a = 3;
+  b = 4;
+  return a + b;
+};
